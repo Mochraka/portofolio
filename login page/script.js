4393375c8c27a0a8d9a669c1419dca9d571e39f9
Mraka <raka194131@gmail.com>
2024-04-22 11:36:15 +0700

    first commit

---
+++ login page/script.js
@@ -0,0 +1,21 @@
+function validateLogin() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var result   = document.getElementById("result")
+
+    // Memeriksa apakah username dan password telah diisi
+    if (username === "" || password === "") {
+        result.innerText = ('Username dan Password harus diisi');
+        return false;
+    } else {
+        // Menggunakan contoh kondisi sederhana untuk memvalidasi login
+        if (username === "raka maulana" && password === "alhamdulillah") {
+            result.innerText = ("Login berhasil!")
+            window.location.href = "../home/index.html";
+            return true;
+        } else {
+            result.innerText = ('Username atau Password salah. Silakan coba lagi.');
+            return false;
+        }
+    }
+}
